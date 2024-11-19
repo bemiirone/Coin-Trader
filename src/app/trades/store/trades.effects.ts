4f74ffd7b21fd71cfe0d174b180494183272612a
Bemi Irone <bemidev@nsmultimedia.co.uk>
2024-11-19 19:16:01 +0000

    add trade after add trade success selector

--- src/app/trades/store/trades.effects.ts
+++ src/app/trades/store/trades.effects.ts
@@ -1,25 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, mergeMap, Observable, of } from 'rxjs';
+import {
+  catchError,
+  map,
+  mergeMap,
+  Observable,
+  of,
+  withLatestFrom,
+} from 'rxjs';
 import { TradeActions } from './trades.actions';
 import { Trade } from '../trades.model';
 import { TradesService } from '../trades.service';
-
-
+import { Store } from '@ngrx/store';
+import { UserActions } from '../../users/store/user.actions';
+import { selectSelectedUser } from '../../users/store/user.selectors';
+import { selectUserTradesValue, selectUserCash } from './trades.selectors';
+import { UserService } from '../../users/user.service';
 
 @Injectable()
 export class TradesEffects {
-  loadTrades$: Observable<ReturnType<typeof TradeActions.loadTradesSuccess | typeof TradeActions.loadTradesFailure>>;
-  addTrade$!: Observable<ReturnType<typeof TradeActions.addTradeSuccess | typeof TradeActions.addTradeFailure>>;
+  loadTrades$: Observable<
+    ReturnType<
+      typeof TradeActions.loadTradesSuccess | typeof TradeActions.loadTradesFailure>>;
+  addTrade$!: Observable<
+    ReturnType<
+      typeof TradeActions.addTradeSuccess | typeof TradeActions.addTradeFailure>>;
+  addTradesSuccess$!: Observable<
+    ReturnType<
+      typeof UserActions.updateUserPortfolio | typeof UserActions.updateUserPortfolioFailure>>;
 
-  constructor(private actions$: Actions, private tradesService: TradesService) {
+  constructor(
+    private actions$: Actions,
+    private tradesService: TradesService,
+    private store: Store,
+    private userService: UserService
+  ) {
     // Load trades effect
     this.loadTrades$ = createEffect(() =>
       this.actions$.pipe(
         ofType(TradeActions.loadTrades),
         mergeMap(() =>
           this.tradesService.getTrades().pipe(
-            map((trades: Trade[]) => TradeActions.loadTradesSuccess({ trades })),
+            map((trades: Trade[]) =>
+              TradeActions.loadTradesSuccess({ trades })
+            ),
             catchError((error) => of(TradeActions.loadTradesFailure({ error })))
           )
         )
@@ -31,12 +55,67 @@ export class TradesEffects {
         ofType(TradeActions.addTrade),
         mergeMap(({ trade }) =>
           this.tradesService.addTrade(trade).pipe(
-            map((addedTrade: Trade) => TradeActions.addTradeSuccess({ trade: addedTrade })),
+            map((addedTrade: Trade) =>
+              TradeActions.addTradeSuccess({ trade: addedTrade })
+            ),
             catchError((error) => of(TradeActions.addTradeFailure({ error })))
           )
         )
       )
     );
+    // Add trade success effect
+    this.addTradesSuccess$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(TradeActions.addTradeSuccess),
+        withLatestFrom(
+          this.store.select(selectSelectedUser),
+          this.store.select(selectUserTradesValue), // Current portfolio value
+          this.store.select(selectUserCash) // Current cash balance
+        ),
+        mergeMap(([{ trade }, user, portfolioValue, cash]) => {
+          if (!user) {
+            return of(
+              UserActions.updateUserPortfolioFailure({
+                error: 'No user selected',
+              })
+            );
+          }
+
+          const updatedCash =
+            trade.order === 'buy'
+              ? cash - trade.amount // Deduct the buy amount from cash
+              : cash + trade.amount; // Add the sell amount to cash
+
+          const tradeValue = trade.volume * trade.price; // Original value of the trade
+          const updatedPortfolioTotal =
+            trade.order === 'buy'
+              ? portfolioValue + tradeValue // Add buy value to portfolio
+              : portfolioValue - tradeValue; // Subtract sell value from portfolio
+
+          return this.userService
+            .updateUserPortfolioAndCash(
+              user._id,
+              updatedPortfolioTotal,
+              updatedCash
+            )
+            .pipe(
+              map(() =>
+                UserActions.updateUserPortfolio({
+                  userId: user._id,
+                  portfolioTotal: updatedPortfolioTotal,
+                  cash: updatedCash,
+                })
+              ),
+              catchError((error) =>
+                of(
+                  UserActions.updateUserPortfolioFailure({
+                    error: error.message,
+                  })
+                )
+              )
+            );
+        })
+      )
+    );
   }
-  
 }
