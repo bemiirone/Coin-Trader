b1e2d17377953fbc802b38da1e16c9993e9c7def
Bemi Irone <bemidev@nsmultimedia.co.uk>
2024-12-03 11:14:39 +0000

    cleanup

--- src/app/coins/store/coins.actions.ts
+++ src/app/coins/store/coins.actions.ts
@@ -6,24 +6,8 @@ import { CoinData, CoinResponse, CoinStatus } from '../coins.model';
 export const CoinsActions = createActionGroup({
   source: 'Coins/API',
   events: {
-    // Action to trigger loading coins
     'Load Coins': emptyProps(), 
-    
-    // Action dispatched when coins are successfully loaded
     'Load CoinsSuccess': props<{ coinsSuccess: CoinResponse }>(),
-
-    // Action dispatched when loading coins fails
     'Load CoinsFailure': props<{ error: string }>(),
-
-    // Other actions
-    'Add Coins': props<{ coins: CoinData }>(),
-    'Upsert Coins': props<{ coins: CoinData }>(),
-    'Add CoinsSuccess': props<{ coinsSuccess: CoinData[] }>(),
-    'Upsert CoinsSuccess': props<{ coinsSuccess: CoinData[] }>(),
-    'Update Coins': props<{ coins: Update<CoinData> }>(),
-    'Update CoinsSuccess': props<{ coinsSuccess: Update<CoinData>[] }>(),
-    'Delete Coins': props<{ id: string }>(),
-    'Delete CoinsSuccess': props<{ ids: string[] }>(),
-    'Clear CoinsSuccess': emptyProps(),
   },
 });
