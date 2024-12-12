b6d7fa221eea45c93d941f22692d1280d6f727d9
Bemi Irone <bemidev@nsmultimedia.co.uk>
2024-12-12 15:19:55 +0000

    Local storage persistence with auth

--- src/app/users/store/auth/auth.actions.ts
+++ src/app/users/store/auth/auth.actions.ts
@@ -4,9 +4,10 @@ import { User } from '../../user.model';
 export const AuthActions = createActionGroup({
   source: 'Auth',
   events: {
-    Login: props<{ email: string; password: string }>(),
+    'Login': props<{ email: string; password: string }>(),
     'Login Success': props<{ user: User; token: string }>(),
     'Login Failure': props<{ error: unknown }>(),
     'Logout': emptyProps(),
+    'Noop': emptyProps(),
   },
 });
--- src/app/users/store/auth/auth.effects.ts
+++ src/app/users/store/auth/auth.effects.ts
@@ -1,25 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthActions } from './auth.actions';
 import { catchError, map, mergeMap, Observable, of, tap } from 'rxjs';
 import { UserService } from '../../user.service';
+import { Store } from '@ngrx/store';
+import { CoinsActions } from '../../../coins/store/coins.actions';
 
 @Injectable()
 export class AuthEffects {
   login$: Observable<ReturnType<typeof AuthActions.loginSuccess | typeof AuthActions.loginFailure>>;
   logout$: Observable<ReturnType<typeof AuthActions.logout>>;
-  constructor(private actions$: Actions, private userService: UserService) {
+  initializeAuth$: Observable<ReturnType<typeof AuthActions.loginSuccess | typeof AuthActions.noop>>;
+  constructor(private actions$: Actions, private userService: UserService, private store: Store) {
+
     this.login$ = createEffect(() =>
-      this.actions$.pipe(
-        ofType(AuthActions.login),
-        mergeMap(({ email, password }) =>
-          this.userService.login(email, password).pipe(
-            map(({ user, token }) => AuthActions.loginSuccess({ user, token })),
-            catchError((error) => of(AuthActions.loginFailure({ error })))
-          )
+    this.actions$.pipe(
+      ofType(AuthActions.login),
+      mergeMap(({ email, password }) =>
+        this.userService.login(email, password).pipe(
+          map(({ user, token }) => AuthActions.loginSuccess({ user, token })),
+          tap(({ user, token }) => {
+            // Save to localStorage
+            localStorage.setItem('authUser', JSON.stringify(user));
+            localStorage.setItem('authToken', token);
+          }),
+          catchError((error) => of(AuthActions.loginFailure({ error })))
         )
       )
-    );
+    )
+  );
 
     this.logout$ = createEffect(() =>
       this.actions$.pipe(
@@ -30,6 +39,30 @@ export class AuthEffects {
         }),
         map(() => AuthActions.logout())
       )
+    );
+
+    this.initializeAuth$ = createEffect(
+      () => {
+        return this.actions$.pipe(
+          ofType(CoinsActions.loadCoinsSuccess),
+          map(() => {
+            if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+              const token = localStorage.getItem('authToken');
+              const user = localStorage.getItem('authUser');
+    
+              if (token && user) {
+                return AuthActions.loginSuccess({ user: JSON.parse(user), token });
+              }
+            }
+            return AuthActions.noop();
+          }),
+          catchError((error) => {
+            console.error('Error initializing auth state:', error);
+            return of(AuthActions.noop());
+          })
+        );
+      },
+      { dispatch: true }
     );
   }
 }
